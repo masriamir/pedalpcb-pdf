877367b0a507af9e584cd13bd4759e585d21e400
Amir Masri <masriamir@gmail.com>
2020-01-19 16:00:43 -0500

    add pdf extraction implementation and related tests

---
+++ src/main/java/com/amirmasri/pedalpcb/pdf/factory/ComponentsFactory.java
@@ -0,0 +1,51 @@
+package com.amirmasri.pedalpcb.pdf.factory;
+
+import com.amirmasri.pedalpcb.pdf.extractor.CapacitorExtractor;
+import com.amirmasri.pedalpcb.pdf.extractor.DiodeExtractor;
+import com.amirmasri.pedalpcb.pdf.extractor.IntegratedCircuitExtractor;
+import com.amirmasri.pedalpcb.pdf.extractor.PotentiometerExtractor;
+import com.amirmasri.pedalpcb.pdf.extractor.ResistorExtractor;
+import com.amirmasri.pedalpcb.pdf.extractor.SwitchExtractor;
+import com.amirmasri.pedalpcb.pdf.extractor.TransistorExtractor;
+import com.amirmasri.pedalpcb.pdf.model.Components;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class ComponentsFactory {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ComponentFactory.class);
+
+  private static final ResistorExtractor EXTRACTOR_RESISTOR = new ResistorExtractor();
+  private static final CapacitorExtractor EXTRACTOR_CAPACITOR = new CapacitorExtractor();
+  private static final TransistorExtractor EXTRACTOR_TRANSISTOR = new TransistorExtractor();
+  private static final DiodeExtractor EXTRACTOR_DIODE = new DiodeExtractor();
+  private static final IntegratedCircuitExtractor EXTRACTOR_INTEGRATED_CIRCUIT = new IntegratedCircuitExtractor();
+  private static final PotentiometerExtractor EXTRACTOR_POTENTIOMETER = new PotentiometerExtractor();
+  private static final SwitchExtractor EXTRACTOR_SWITCH = new SwitchExtractor();
+
+  public Components create(String text) {
+    Components components = new Components();
+
+    components.addResistors(EXTRACTOR_RESISTOR.extract(text));
+    LOGGER.debug("resistors = {}", components.getResistors());
+
+    components.addCapacitors(EXTRACTOR_CAPACITOR.extract(text));
+    LOGGER.debug("capacitors = {}", components.getCapacitors());
+
+    components.addTransistors(EXTRACTOR_TRANSISTOR.extract(text));
+    LOGGER.debug("transistors = {}", components.getTransistors());
+
+    components.addDiodes(EXTRACTOR_DIODE.extract(text));
+    LOGGER.debug("diodes = {}", components.getDiodes());
+
+    components.addIntegratedCircuits(EXTRACTOR_INTEGRATED_CIRCUIT.extract(text));
+    LOGGER.debug("integrated circuits = {}", components.getIntegratedCircuits());
+
+    components.addPotentiometers(EXTRACTOR_POTENTIOMETER.extract(text));
+    LOGGER.debug("potentiometers = {}", components.getPotentiometers());
+
+    components.addSwitches(EXTRACTOR_SWITCH.extract(text));
+    LOGGER.debug("switches = {}", components.getSwitches());
+
+    return components;
+  }
+}
---
+++ src/main/java/com/amirmasri/pedalpcb/pdf/util/DocumentUtils.java
@@ -0,0 +1,65 @@
+package com.amirmasri.pedalpcb.pdf.util;
+
+import com.amirmasri.pedalpcb.pdf.exception.PedalPcbPdfException;
+import java.io.File;
+import java.io.IOException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.encryption.AccessPermission;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class DocumentUtils {
+  private static final Logger LOGGER = LoggerFactory.getLogger(DocumentUtils.class);
+
+  private DocumentUtils() {
+  }
+
+  public static PDDocument loadDocument(String filePath) throws PedalPcbPdfException {
+    try {
+      PDDocument document = PDDocument.load(new File(filePath));
+      AccessPermission permission = document.getCurrentAccessPermission();
+
+      if (!permission.canExtractContent()) {
+        throw new PedalPcbPdfException("invalid permissions to read document");
+      }
+
+      return document;
+    } catch (IOException e) {
+      throw new PedalPcbPdfException("unable to load document", e);
+    }
+  }
+
+  public static String extractDocumentText(PDDocument document) throws PedalPcbPdfException {
+    if (document.isEncrypted()) {
+      throw new PedalPcbPdfException("cannot extract text from encrypted document");
+    }
+
+    PDFTextStripper stripper;
+
+    try {
+      stripper = new PDFTextStripper();
+    } catch (IOException e) {
+      LOGGER.error(e.getMessage(), e);
+      throw new PedalPcbPdfException("unable to create new PDFTextStripper", e);
+    }
+
+    stripper.setStartPage(1);
+    stripper.setEndPage(document.getNumberOfPages());
+
+    try {
+      return stripper.getText(document);
+    } catch (IOException e) {
+      LOGGER.error(e.getMessage(), e);
+      throw new PedalPcbPdfException("unable to extract text from document", e);
+    }
+  }
+
+  public static void closeDocument(PDDocument document) {
+    try {
+      document.close();
+    } catch (IOException e) {
+      LOGGER.warn(e.getMessage(), e);
+    }
+  }
+}
